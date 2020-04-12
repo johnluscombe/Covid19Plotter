49e9d7296e3c825327ccb3a1dd6a10b3ab0434ac
John Luscombe <jluscombe96@gmail.com>
2020-04-12 16:09:32 -0400

    Restructuring

--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-BASE_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_%s_US.csv"
-
-CONFIRMED_DF = pd.read_csv(BASE_URL % "confirmed")
-DEATHS_DF = pd.read_csv(BASE_URL % "deaths")
+BASE_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data" \
+           "/csse_covid_19_time_series/time_series_covid19_%s_US.csv "
 
 TOTAL_CONFIRMED_MODE = 1
 NEW_CONFIRMED_MODE = 2
@@ -14,8 +12,6 @@ NEW_DEATHS_MODE = 4
 # 3/1/20
 STARTING_DAY = 43
 
-LAST_UPDATED = CONFIRMED_DF.columns[-1]
-
 TOTAL_CASES = "Total Cases"
 NEW_CASES = "New Cases"
 
@@ -23,15 +19,35 @@ STATE = "Province_State"
 COUNTY = "Admin2"
 
 
+def array_to_lower_case(arr):
+    for i in range(len(arr)):
+        arr[i] = arr[i].lower()
+    return arr
+
+
+def get_array_diffs(arr):
+    if len(arr) > 0:
+        diffs = arr[:1]
+        for i in range(1, len(arr)):
+            diffs.append(arr[i] - arr[i - 1])
+        return diffs
+    return []
+
+
 class Covid19Plotter:
+    def __init__(self):
+        print("Loading...")
+        self.confirmed_df = pd.read_csv(BASE_URL % "confirmed")
+        self.deaths_df = pd.read_csv(BASE_URL % "deaths")
+
     def plot(self):
         while True:
             mode = self._get_mode()
             state = self._get_state(mode)
 
             if state != "":
-                confirmed_df = CONFIRMED_DF[CONFIRMED_DF[STATE] == state]
-                deaths_df = DEATHS_DF[DEATHS_DF[STATE] == state]
+                confirmed_df = self.confirmed_df[self.confirmed_df[STATE] == state]
+                deaths_df = self.deaths_df[self.deaths_df[STATE] == state]
 
                 if mode == TOTAL_CONFIRMED_MODE:
                     self._plot_total(confirmed_df, state, "Total Confirmed")
@@ -43,13 +59,13 @@ class Covid19Plotter:
                     self._plot_new(deaths_df, state, "New Deaths")
             else:
                 if mode == TOTAL_CONFIRMED_MODE:
-                    self._plot_total(CONFIRMED_DF, "US", "Total Confirmed")
+                    self._plot_total(self.confirmed_df, "US", "Total Confirmed")
                 elif mode == NEW_CONFIRMED_MODE:
-                    self._plot_new(CONFIRMED_DF, "US", "New Confirmed")
+                    self._plot_new(self.confirmed_df, "US", "New Confirmed")
                 elif mode == TOTAL_DEATHS_MODE:
-                    self._plot_total(DEATHS_DF, "US", "Total Deaths")
+                    self._plot_total(self.deaths_df, "US", "Total Deaths")
                 elif mode == NEW_DEATHS_MODE:
-                    self._plot_new(DEATHS_DF, "US", "New Deaths")
+                    self._plot_new(self.deaths_df, "US", "New Deaths")
     
     def _get_mode(self):
         print("What type of data do you want to view?")
@@ -71,23 +87,18 @@ class Covid19Plotter:
         print("What state do you want to view? (Just press ENTER to see whole country)")
 
         if mode == TOTAL_DEATHS_MODE or mode == NEW_DEATHS_MODE:
-            df = DEATHS_DF
+            df = self.deaths_df
         else:
-            df = CONFIRMED_DF
+            df = self.confirmed_df
 
         state = self._input()
-        states = self._array_to_lower_case(df[STATE].tolist())
+        states = array_to_lower_case(df[STATE].tolist())
 
         while state != "" and state.lower() not in states:
             print("Invalid state.")
             state = self._input()
 
         return state
-
-    def _array_to_lower_case(self, arr):
-        for i in range(len(arr)):
-            arr[i] = arr[i].lower()
-        return arr
     
     def _input(self):
         i = input(">>> ")
@@ -96,35 +107,33 @@ class Covid19Plotter:
         return i
     
     def _plot_total(self, df, location, data_desc):
-        lst = df.sum()[STARTING_DAY:].values.tolist()
-        self._plot(lst, location, data_desc)
+        last_updated = df.columns[-1]
+        df = df.sum()[STARTING_DAY:]
+
+        self._plot(df.values.tolist(), location, last_updated, data_desc)
     
     def _plot_new(self, df, location, data_desc):
-        lst = df.sum()[STARTING_DAY:].values.tolist()
-        self._plot(self._get_diffs(lst), location, data_desc)
+        last_updated = df.columns[-1]
+        df = df.sum()[STARTING_DAY:]
+        lst = get_array_diffs(df.values.tolist())
+
+        self._plot(lst, location, last_updated, data_desc)
     
-    def _plot(self, conf_list, location, data_desc):
-        title = "%s %s (Last Updated %s)" % (location, data_desc, LAST_UPDATED)
+    def _plot(self, data, location, last_updated, data_desc):
+        title = "%s %s (Last Updated %s)" % (location, data_desc, last_updated)
 
         plt.figure(num=title)
-        plt.plot(conf_list)
+        plt.plot(data)
         plt.xlabel("Days Since 3/1/20")
         plt.ylabel(data_desc)
         plt.suptitle(title)
-        plt.title("%s: %s" % (data_desc, conf_list[-1]), size=8)
+        plt.title("%s: %s" % (data_desc, data[-1]), size=8)
         plt.grid()
         plt.show()
 
     def _get_state_confirmed_data(self, state):
-        return CONFIRMED_DF[CONFIRMED_DF[STATE] == state].sum()[STARTING_DAY:]
-    
-    def _get_diffs(self, arr):
-        if len(arr) > 0:
-            diffs = arr[:1]
-            for i in range(1, len(arr)):
-                diffs.append(arr[i] - arr[i-1])
-            return diffs
-        return []
+        df = self.confirmed_df
+        return df[df[STATE] == state].sum()[STARTING_DAY:]
 
 
 plotter = Covid19Plotter()
